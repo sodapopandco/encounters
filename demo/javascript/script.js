fe627f83cced286833a755e332817fc5906b38a2
James Goode <james@sodapopandco.com>
2013-06-15 21:51:05 +1000

    Update the HTML example

--- demo/javascript/script.js
+++ demo/javascript/script.js
@@ -1,6 +1,16 @@
 // Toggle between the dark and light themes
 $("#toggle").click(function() {
   $("body").toggleClass("dark").toggleClass("light");
+
+  // Swap the example body class
+  var swap = $(".swap").html()
+  if (swap == "light") {
+    $(".swap").html("dark");
+  }
+  else if (swap == "dark") {
+    $(".swap").html("light");
+  }
+
   return false;
 });
 
