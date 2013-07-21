aecdd5f2f65c120795be062dcf8beca71b247da9
James Goode <james@sodapopandco.com>
2013-07-21 17:35:00 +1000

    Add parrallax to the stars background

--- demo/javascript/script.js
+++ demo/javascript/script.js
@@ -1,20 +1,28 @@
-// Toggle between the dark and light themes
-$("#toggle").click(function() {
-  $("body").toggleClass("dark").toggleClass("light");
+$(document).ready(function () {
+  // Toggle between the dark and light themes
+  $("#toggle").click(function() {
+    $("body").toggleClass("dark").toggleClass("light");
 
-  // Swap the example body class
-  var swap = $(".swap").html()
-  if (swap == "light") {
-    $(".swap").html("dark");
-  }
-  else if (swap == "dark") {
-    $(".swap").html("light");
-  }
+    // Swap the example body class
+    var swap = $(".swap").html()
+    if (swap == "light") {
+      $(".swap").html("dark");
+    }
+    else if (swap == "dark") {
+      $(".swap").html("light");
+    }
 
-  return false;
-});
+    return false;
+  });
 
-// Pulse the contrast icon until someone hovers over it
-$("#toggle").hover(function() {
-  $(this).removeClass("pulse");
-});
+  $(window).scroll(function() {
+    $(".stars-1").css("transform","translate3d(0px," + -($(window).scrollTop()/5) + "px, 0px)");
+    $(".stars-2").css("transform","translate3d(0px," + -($(window).scrollTop()/3) + "px, 0px)");
+    $(".stars-3").css("transform","translate3d(0px," + -($(window).scrollTop()/2) + "px, 0px)");
+  });
+
+  // Pulse the contrast icon until someone hovers over it
+  $("#toggle").hover(function() {
+    $(this).removeClass("pulse");
+  });
+})
